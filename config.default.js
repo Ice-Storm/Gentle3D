9c1ff6bd260f88f3524aa1f637e8d4f5ec971534
wcwz020140 <523003801@qq.com>
2016-03-20 15:27:29 +0800

    2.1Beta
    调整代码书写格式

--- config.default.js
+++ config.default.js
@@ -1,21 +1,21 @@
 module.exports = {
-	port: 3000,
-	key: '123456',
-	userName: 'admin',
-	userPassword: 'admin',
-	database: {
-		user: 'root',
-		password: null,
-		database: '3dweb',
-	  host: 'localhost',
-	  dialect: 'mysql',
-	  charset: 'utf8',
-	  resetDB: true,
-	  collate: 'utf8_general_ci', //排序时根据utf8变码格式来排序
-	  pool: {
-	    max: 20,
-	    min: 15,
-	    idle: 10000 //多少毫秒释放MYSQL链接
-	  }
-	}
+  port: 3000,
+  key: '123456',
+  userName: 'admin',
+  userPassword: 'admin',
+  database: {
+    user: 'root',
+    password: null,
+    database: '3dweb',
+    host: 'localhost',
+    dialect: 'mysql',
+    charset: 'utf8',
+    resetDB: true,
+    collate: 'utf8_general_ci', //排序时根据utf8变码格式来排序
+    pool: {
+      max: 20,
+      min: 15,
+      idle: 10000 //多少毫秒释放MYSQL链接
+    }
+  }
 }
